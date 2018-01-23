ef74bf4dbf9899e860a036e7e57ebbc2ff386da7
itas-gmbh <info@itas.cc>
2018-01-24 00:16:45 +0100

    Create index.php

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+/*
+ * Plugin Name: Woocommerce Licence Manager
+ * Version: 1.0.0
+ * Description: This plugin helps to sell software license codes over woocommerce - when a predefined article is bought, a serial code is sent to the customer and an entry into the db is made
+ * Author: Michael Allram
+ * Author URI: http://itas.cc
+ * Plugin URI: https://github.com/itas-gmbh/wc_licence_manager
+ * Text Domain: wc_licence_manager
+ * License: MIT
+ * License URI: https://opensource.org/licenses/MIT
+*/
+
