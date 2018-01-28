9d774010a0def509985c81f6a8be1f2c4ff59f2f
itas-gmbh <info@itas.cc>
2018-01-28 01:19:37 +0100

    1

--- index.php
+++ index.php
@@ -13,8 +13,8 @@
 
 //add_action( 'plugins_loaded', 'add_voucher', 10, 1 );
 //add_action('plugins_loaded', 'test');
-//add_action( 'woocommerce_payment_complete', 'add_voucher', 10, 1 );
-add_action('woocommerce_order_status_changed', 'add_voucher');
+add_action( 'woocommerce_order_status_changed', 'add_voucher', 10, 1 );
+//add_action('woocommerce_order_status_changed', 'add_voucher');
 
 
 function test(){
