ef14c77147fd6069238969992b7a303ec7800bf5
itas-gmbh <info@itas.cc>
2018-01-29 02:29:00 +0100

    Update index.php

--- index.php
+++ index.php
@@ -94,7 +94,8 @@ dbDelta( $sql );
 
 
 
-add_action( 'woocommerce_order_status_changed', 'add_voucher', 10, 1);
+//add_action( 'woocommerce_order_status_changed', 'add_voucher', 10, 1);
+add_action( 'woocommerce_payment_complete', 'add_voucher', 10, 1);
 
 
 
