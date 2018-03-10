1349a612cf5efb9b7576df3a0fbfd90e8a9a57ac
itas-gmbh <info@itas.cc>
2018-03-10 22:25:23 +0100

    Update cron.php

--- cron.php
+++ cron.php
@@ -1,32 +1,4 @@
 <?php
-/*
- * Plugin Name: Woocommerce Licence Manager
- * Version: 1.0.0
- * Description: This plugin helps to sell software license codes over woocommerce - when a predefined article is bought, a serial code is sent to the customer and an entry into the db is made
- * Author: Michael Allram
- * Author URI: http://itas.cc
- * Plugin URI: https://github.com/itas-gmbh/wc_licence_manager
- * Text Domain: wc_licence_manager
- * License: MIT
- * License URI: https://opensource.org/licenses/MIT
-*/
-
-//add_action( 'plugins_loaded', 'add_voucher', 10, 1 );
-//add_action('plugins_loaded', 'test');
-
-//add_action('woocommerce_order_status_changed', 'add_voucher');
-
-
-function test(){
-	echo "<script language='javascript'>alert('test successful');</script>";
-}
-
-function init(){
-//create Database	
-	
-	
-	
-}
 
 function dispatch_licenses(){ //call every 15 minutes
 	
@@ -46,29 +18,6 @@ function dispatch_licenses(){ //call every 15 minutes
 
 
 function add_voucher($order_id){
-	//echo "<script language='javascript'>alert('payment received');</script>";
-	error_log("***Plugin started***");
-	/* create table */
-	global $wpdb;
-
-	$table_name = $wpdb->prefix . "licenses";
-
-	$charset_collate = $wpdb->get_charset_collate();
-
-	$sql = "CREATE TABLE IF NOT EXISTS $table_name (
-	id int NOT NULL AUTO_INCREMENT,
-	art_id int,
-	art_desc varchar(200),
-	serial varchar(500),
-	used_by int,
-	order_id int,
-	comment varchar(5000),
-	created datetime,
-	PRIMARY KEY  (id)
-	) $charset_collate;";
-
-require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
-dbDelta( $sql );	
 	
 	
 	/* get product id */
